d1cf1f3127648147c611ba2fa20fc2aeb7afe464
simonbru <simon@brulhart.me>
2017-04-13 11:05:57 +0200

    Add offensive fortune in title

--- mumble-online.py
+++ mumble-online.py
@@ -6,16 +6,22 @@ import os
 import sys
 import time
 from datetime import datetime
+from subprocess import check_output
 
 import mice
 
+
+raw_fortune = check_output('/usr/games/fortune -s -n 50 -o'.split(' '))
+fortune = ', '.join(
+	line.strip() for line in raw_fortune.split('\n') if line.strip()
+)
 
 while True:
 	server = mice.m.getServer(1)
 	channels = server.getChannels()
 	users = server.getUsers().values()
 	os.system('clear')
-	print('Rababou - {:%H:%M:%S}'.format(datetime.now()))
+	print('Rababou - {:%H:%M:%S} ~ {}'.format(datetime.now(), fortune))
 	if not users:
 		print(u"No one here ¯\_(ツ)_/¯")
 	for user in sorted(users, key=lambda u: u.channel):
