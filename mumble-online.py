2f1cd03cd0e93911765e5af36a7da285dcdd055a
simonbru <simon@brulhart.me>
2017-04-13 13:20:07 +0200

    Add tree view of channels

--- mumble-online.py
+++ mumble-online.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8
 from __future__ import print_function
 
+import itertools
 import os
 import sys
 import time
@@ -9,6 +10,36 @@ from datetime import datetime
 from subprocess import check_output
 
 import mice
+
+
+def group_by(iterable, key):
+	data = sorted(iterable, key=key)
+	return dict(
+		(key, list(group))
+		for key, group in itertools.groupby(data, key)
+	)
+
+
+def format_chan(channel, channels_by_parent, users_by_chan):
+	yield channel.name
+	for user in users_by_chan.get(channel.id, []):
+		yield u'├─ ' + format_user(user)
+	for child_chan in channels_by_parent.get(channel.id, []):
+		child_lines = list(format_chan(
+			child_chan, channels_by_parent, users_by_chan
+		))
+		yield u'├─ ' + child_lines[0].decode('utf8')
+		for line in child_lines[1:]:
+			yield u'│  ' + line
+
+
+def format_user(user):
+	status = ''
+	if user.selfDeaf:
+		status = ' [deaf]'
+	elif user.selfMute:
+		status = ' [mute]'
+	return '{}{}'.format(user.name.decode('utf8'), status)
 
 
 raw_fortune = check_output('/usr/games/fortune -s -n 50 -o'.split(' '))
@@ -22,14 +53,13 @@ while True:
 	users = server.getUsers().values()
 	os.system('clear')
 	print('Rababou - {:%H:%M:%S} ~ {}'.format(datetime.now(), fortune))
-	if not users:
-		print(u"No one here ¯\_(ツ)_/¯")
-	for user in sorted(users, key=lambda u: u.channel):
-		status = ''
-		if user.selfDeaf:
-			status = ' [deaf]'
-		elif user.selfMute:
-			status = ' [mute]'
-		chan = channels[user.channel].name
-		print('{}{} | {}'.format(user.name, status, chan))
+	print()
+	channels_by_parent = group_by(
+		channels.values(), key=lambda c: c.parent
+	)
+	users_by_chan = group_by(users, key=lambda u: u.channel)
+	print(
+		*format_chan(channels[0], channels_by_parent, users_by_chan),
+		sep='\n'
+	)
 	time.sleep(5)
