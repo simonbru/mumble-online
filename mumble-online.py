fbba023d60e28659fdbac3c26bfb203cbc744d49
simonbru <simon@brulhart.me>
2017-04-13 10:37:57 +0200

    Improve formatting and show more details

--- mumble-online.py
+++ mumble-online.py
@@ -1,8 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
+# -*- coding: utf-8
 from __future__ import print_function
-import time, mice, sys
+
+import os
+import sys
+import time
+from datetime import datetime
+
+import mice
+
 
 while True:
-	time.sleep(1)
-	print('\r', [u.name for u in mice.m.getServer(1).getUsers().values()], end='')
-	sys.stdout.flush()
+	server = mice.m.getServer(1)
+	channels = server.getChannels()
+	users = server.getUsers().values()
+	os.system('clear')
+	print('Rababou - {:%H:%M:%S}'.format(datetime.now()))
+	if not users:
+		print(u"No one here ¯\_(ツ)_/¯")
+	for user in sorted(users, key=lambda u: u.channel):
+		status = ''
+		if user.selfDeaf:
+			status = ' [deaf]'
+		elif user.selfMute:
+			status = ' [mute]'
+		chan = channels[user.channel].name
+		print('{}{} | {}'.format(user.name, status, chan))
+	time.sleep(5)
